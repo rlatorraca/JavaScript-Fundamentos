39d27fe6b0a03bb34e99d2053249af7efeeb73a2
Rodrigo Latorraca <rlatorraca@gmail.com>
2022-03-01 00:41:24 -0400

    exemplos de comentarios, undefined, null, listas, objects.

---
+++ Data type - part 02/demo04-comentarios.js
@@ -0,0 +1,8 @@
+// comentário em uma unica linha 
+
+/**
+ * Author:  Rodrigo Latorraca
+ */
+ // ignorando a linha abaixo
+ // const variavelConstante = 1+2+4+4
+ const variavelConstante = 1 + 4 // comentando na linha
---
+++ Data type - part 02/demo05-undefined-e-null.js
@@ -0,0 +1,22 @@
+let variavelSemInicializacao
+
+// UNDEFINED = nao recebeu valor / nada foi definido. Tipo UNDEFINED e Valor UNDEFINED
+console.log(
+    variavelSemInicializacao,
+    typeof(variavelSemInicializacao)
+)
+
+
+variavelSemInicializacao = "Hello world!!"
+console.log(
+    variavelSemInicializacao,
+    typeof(variavelSemInicializacao)
+)
+
+// NULL = é um OBJECT , com valor de null
+variavelSemInicializacao = null   
+console.log(
+    variavelSemInicializacao,
+    typeof(variavelSemInicializacao)
+)
+
