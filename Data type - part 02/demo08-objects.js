5935ef31c8c7de813967f6ba2fd76eb2f36f023e
Rodrigo Latorraca <rlatorraca@gmail.com>
2022-03-06 14:50:07 -0400

    Changes into demo08

--- Data type - part 02/demo08-objects.js
+++ Data type - part 02/demo08-objects.js
@@ -1,27 +1,33 @@
 const heroi = {
+    
+    /* JSON - Java Script Objet notation */
+    // Key : valeu
+    
     nome: 'Flash',
     idade: 100,
     sexo: 'Masculino'
 }
 
-// console.log( 'nome', heroi.nome)
-// console.log('idade', heroi['idade'])
-// console.log('sexo', heroi.sexo)
-// console.log('naoExiste', heroi.naoExiste)
-// heroi.id = 0001
-// console.log(heroi)
+console.log( 'nome', heroi.nome)
+console.log('idade', heroi['idade'])
+console.log('sexo', heroi.sexo)
+console.log('naoExiste', heroi.naoExiste)
+heroi.id = 0001
+console.log(heroi)
+heroi.naoExiste = 'Nao existe valor';
 
-// saber chaves
-// console.log(Object.keys(heroi))
+// Mostra chaves
+console.log('Object.keys(heroi)' ,Object.keys(heroi))
 
-// saber valores
-// console.log(Object.values(heroi))
+// <> Mostra valores
+console.log('Object.values(heroi)', Object.values(heroi))
 
-// juntar dois objetos
+// <> Juntar dois objetos
 const pessoa = {
     tamanho: '10 metros'
 }
 const novoObj = Object.assign(heroi, pessoa)
+console.log('novoObj', novoObj)
 delete novoObj.nome
-console.log(novoObj)
+console.log('delete novoObj.nome', novoObj)
 
