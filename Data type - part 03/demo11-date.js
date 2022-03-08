495e53c23d1e308a59b162513cf430078198075f
Rodrigo Latorraca <rlatorraca@gmail.com>
2022-03-07 23:13:35 -0400

    Changes on dates examples

--- Data type - part 03/demo11-date.js
+++ Data type - part 03/demo11-date.js
@@ -1,16 +1,16 @@
 // meses começam do zero!
 const dataAniversario = new Date(2020, 0, 20)
-// console.log(dataAniversario)
+console.log(dataAniversario)
 
 const primeiraDataDoJS = new Date(0)
-// console.log(primeiraDataDoJS.getTime())
+console.log(primeiraDataDoJS.getTime())
 
 const hoje = new Date()
-// console.log(hoje.toString())
-// console.log(hoje.toLocaleDateString())
+console.log(hoje.toString())
+console.log(hoje.toLocaleDateString())
 
 // formato global recomendado!
-// console.log(hoje.toISOString())
+console.log(hoje.toISOString())
 
 const dia = hoje.getDate()
 hoje.setDate(dia + 5 ) // +5 dias depois de hoje
